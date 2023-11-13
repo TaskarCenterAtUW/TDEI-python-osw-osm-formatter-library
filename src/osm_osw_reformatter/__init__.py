2de43404e6fa0d45da98c90c26b212b125fcae49
Anuj <sujatam@gaussiansolutions.com>
2023-11-13 15:34:19 +0530

    - Added init files  - Updated version  - Added prefix

--- src/osm_osw_reformatter/__init__.py
+++ src/osm_osw_reformatter/__init__.py
@@ -10,22 +10,23 @@ DOWNLOAD_FOLDER = f'{Path.cwd()}/tmp'
 
 
 class Formatter:
-    def __init__(self, workdir=DOWNLOAD_FOLDER, file_path=None):
+    def __init__(self, workdir=DOWNLOAD_FOLDER, file_path=None, prefix='final'):
         is_exists = os.path.exists(workdir)
         if not is_exists:
             os.makedirs(workdir)
         self.workdir = workdir
         self.file_path = file_path
         self.generated_files = []
+        self.prefix = prefix
 
     async def osm2osw(self) -> Response:
-        convert = OSM2OSW(pbf_file=self.file_path, workdir=self.workdir, prefix='final')
+        convert = OSM2OSW(pbf_file=self.file_path, workdir=self.workdir, prefix=self.prefix)
         result = await convert.convert()
         self.generated_files = result.generated_files
         return result
 
     def osw2osm(self) -> Response:
-        convert = OSW2OSM(zip_file_path=self.file_path, workdir=self.workdir, prefix='final')
+        convert = OSW2OSM(zip_file_path=self.file_path, workdir=self.workdir, prefix=self.prefix)
         result = convert.convert()
         self.generated_files = [result.generated_files]
         return result
